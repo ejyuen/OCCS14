30db4b5a64952cff1263f276dca53a6689ebb361
AndyMei <AndyMei@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2013-09-30 23:44:42 +0000

    herpderpderp

---
+++ Pong Legacy/src/alpha/ClientUI.java
@@ -0,0 +1,45 @@
+package alpha;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+
+
+public class ClientUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	public ClientUI() {
+		setTitle("Pong Legacy | Prototype v0.1.0");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 500);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnConnect = new JButton("Connect");
+		btnConnect.setBounds(47, 400, 200, 25);
+		contentPane.add(btnConnect);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setBounds(117, 436, 60, 25);
+		contentPane.add(btnBack);
+		
+		JRadioButton rdbtnSelectAServer = new JRadioButton("Select a server from the list:");
+		rdbtnSelectAServer.setBounds(66, 25, 161, 25);
+		contentPane.add(rdbtnSelectAServer);
+		
+		JRadioButton rdbtnManualConnection = new JRadioButton("Manual Connection:");
+		rdbtnManualConnection.setBounds(87, 325, 120, 25);
+		contentPane.add(rdbtnManualConnection);
+		
+		textField = new JTextField();
+		textField.setBounds(47, 355, 200, 25);
+		contentPane.add(textField);
+		textField.setColumns(10);
+	}
+}
