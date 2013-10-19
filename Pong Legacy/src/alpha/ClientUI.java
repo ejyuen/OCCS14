53e4391fa59a75565b95de826eae3543d38c64a8
AndyMei <AndyMei@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2013-10-19 20:53:12 +0000

    radio buttons now work

--- Pong Legacy/src/alpha/ClientUI.java
+++ Pong Legacy/src/alpha/ClientUI.java
@@ -8,12 +8,11 @@ import javax.swing.border.EmptyBorder;
 
 import alpha.communicator.Client;
 
-
-
 public class ClientUI extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
+	private JRadioButton rdbtnSelectAServer, rdbtnManualConnection;
 
 	public ClientUI() {
 		setTitle("Pong Legacy | Prototype v0.2.1");
@@ -25,18 +24,7 @@ public class ClientUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JButton btnConnect = new JButton("Connect");
-		btnConnect.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				String ipAddress = textField.getText();
-				new Pong(new Client(ipAddress));
-				setVisible(false);
-			}
-		});
-		btnConnect.setBounds(47, 400, 200, 25);
-		contentPane.add(btnConnect);
-		
+
 		JButton btnExit = new JButton("Exit");
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -45,18 +33,43 @@ public class ClientUI extends JFrame {
 		});
 		btnExit.setBounds(117, 436, 60, 25);
 		contentPane.add(btnExit);
-		
-		JRadioButton rdbtnSelectAServer = new JRadioButton("Select a server from the list:");
+
+		rdbtnSelectAServer = new JRadioButton("Select a server from the list:");
 		rdbtnSelectAServer.setBounds(66, 25, 161, 25);
 		contentPane.add(rdbtnSelectAServer);
-		
-		JRadioButton rdbtnManualConnection = new JRadioButton("Manual Connection:");
+
+		rdbtnManualConnection = new JRadioButton("Manual Connection:");
 		rdbtnManualConnection.setBounds(87, 325, 120, 25);
 		contentPane.add(rdbtnManualConnection);
 		
+		buttonGroup();
+
 		textField = new JTextField();
 		textField.setBounds(47, 355, 200, 25);
 		contentPane.add(textField);
 		textField.setColumns(10);
+
+		JButton btnConnect = new JButton("Connect");
+		btnConnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String ipAddress = null;
+				if (rdbtnManualConnection.isSelected()) {
+					ipAddress = textField.getText();
+				}else if(rdbtnSelectAServer.isSelected()){
+					ipAddress = "127.0.0.1"; //something i will add later
+				}
+
+				new Pong(new Client(ipAddress));
+				setVisible(false);
+			}
+		});
+		btnConnect.setBounds(47, 400, 200, 25);
+		contentPane.add(btnConnect);
+	}
+	
+	private void buttonGroup(){
+		ButtonGroup serverSelect = new ButtonGroup();
+		serverSelect.add(rdbtnManualConnection);
+		serverSelect.add(rdbtnSelectAServer);
 	}
 }
