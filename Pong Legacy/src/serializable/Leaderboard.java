948864a4e2a317808f51a6824febe90ccdecfc33
MatthewLi <MatthewLi@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2013-11-16 22:04:17 +0000

    got rid of useless imports

--- Pong Legacy/src/serializable/Leaderboard.java
+++ Pong Legacy/src/serializable/Leaderboard.java
@@ -1,15 +1,5 @@
 package serializable;
 import serializable.Score;
-import com.google.gdata.client.authn.oauth.*;
-import com.google.gdata.client.spreadsheet.*;
-import com.google.gdata.data.*;
-import com.google.gdata.data.batch.*;
-import com.google.gdata.data.spreadsheet.*;
-import com.google.gdata.util.*;
-
-import java.io.IOException;
-import java.net.*;
-import java.util.*;
 
 public class Leaderboard extends Score{
 
