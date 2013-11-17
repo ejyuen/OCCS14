15af0f500e1ffbc9368c78ef35aeac3a9cfb4045
MatthewLi <MatthewLi@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2013-11-17 00:54:48 +0000

    changed flush to reset

--- Pong Legacy/src/communicator/Client.java
+++ Pong Legacy/src/communicator/Client.java
@@ -39,7 +39,7 @@ public class Client implements Communicator{
 	
 	public void sendObject(Object o){
 		try {
-			objOutput.flush();
+			objOutput.reset();
 			objOutput.writeUnshared(o);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
