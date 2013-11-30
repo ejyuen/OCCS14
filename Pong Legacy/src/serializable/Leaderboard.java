ed9202dcd2944f4ff9c77d6d7a3f8bc13dbc9ce1
NeilShah <NeilShah@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2013-11-30 15:20:36 +0000

    Added MySQL (I think) to the build path

--- Pong Legacy/src/serializable/Leaderboard.java
+++ Pong Legacy/src/serializable/Leaderboard.java
@@ -1,9 +1,16 @@
 package serializable;
+import java.util.*;
+import java.io.IOException;
+import java.net.*;
+
 import serializable.Score;
 
-import com.google.gdata.data.extensions.*;
-import com.google.gdata.data.spreadsheet.*;
+import com.google.gdata.client.authn.oauth.*;
 import com.google.gdata.client.spreadsheet.*;
+import com.google.gdata.data.*;
+import com.google.gdata.data.batch.*;
+import com.google.gdata.data.spreadsheet.*;
+import com.google.gdata.util.*;
 
 
 
@@ -13,16 +20,53 @@ public class Leaderboard extends Score{
 	 * 
 	 */
 	private static final long serialVersionUID = 7683903530289737107L;
-
-	public static void main(String[] args) {
-
-		SpreadsheetService leaderboard = new SpreadsheetService("Leaderboard");
-		SpreadsheetQuery query = new SpreadsheetQuery(null);
-//		SpreadsheetFeed feed = leaderboard.Query(query);	
-	}
 	
+		    
 	public Leaderboard(int numPlayers) {
 		super(numPlayers);
 		// TODO Auto-generated constructor stub
 	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////
+	// Ignore! -Neil                                                                   //
+	/////////////////////////////////////////////////////////////////////////////////////
+	
+	/*public static void main(String[] args) throws AuthenticationException, MalformedURLException, IOException, ServiceException, OAuthException {
+
+		Boolean USE_RSA_SIGNING = false;
+		String CLIENT_ID = "523743008688.apps.googleusercontent.com";
+		String CLIENT_SECRET = "DyrdTwZ1JLmf486av1k_MC6n";
+		String SCOPE = "https://spreadsheets.google.com/feeds";
+		
+		GoogleOAuthParameters oauthParameters = new GoogleOAuthParameters();
+		oauthParameters.setOAuthConsumerKey(CLIENT_ID);
+	
+		OAuthSigner signer;
+	    if (USE_RSA_SIGNING) {
+	      signer = new OAuthRsaSha1Signer(CLIENT_SECRET);
+	    } else {
+	      oauthParameters.setOAuthConsumerSecret(CLIENT_SECRET);
+	      signer = new OAuthHmacSha1Signer();
+	    }
+		GoogleOAuthHelper oauthHelper = new GoogleOAuthHelper(signer);
+		
+		oauthParameters.setScope(SCOPE);
+	    
+	    SpreadsheetService service = new SpreadsheetService("Leaderboards");
+	    URL LEADERBOARDS_FEED_URL = new URL("https://spreadsheets.google.com/feeds/spreadsheets/public/basic");
+
+	    // Make a request to the API and get all spreadsheets.
+	    SpreadsheetFeed feed = service.getFeed(LEADERBOARDS_FEED_URL, SpreadsheetFeed.class);
+	    List<SpreadsheetEntry> spreadsheets = feed.getEntries();
+
+	    // Iterate through all of the spreadsheets returned
+	    for (SpreadsheetEntry spreadsheet : spreadsheets) {
+	      // Print the title of this spreadsheet to the screen
+	      System.out.println(spreadsheet.getTitle().getPlainText());
+        
+        
+     }
+     
+}*/
+	
 }
