ad4518ddfcac95e200aa542dbe78b6fcfb3a6e70
MatthewLi <MatthewLi@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-01-28 18:43:29 +0000

    made some basic changes to the way property works

--- Monopoly/src/host/property/Property.java
+++ Monopoly/src/host/property/Property.java
@@ -7,11 +7,21 @@ public class Property {
 	private int propertyID; //look in PropertyList for which number corresponds to which property
 	private int houses;
 	
+	public Property(int id){
+		propertyID = id;
+		houses = 0;
+		owner = null;
+	}
+	
 	/**
 	 * 
 	 * @return how much you should pay for landing on this space
 	 */
 	public double payment(){ //implement this later, remember that there are special cases
 		return 1.0;
+	}
+	
+	public Company getOwner(){
+		return owner;
 	}
 }
--- Monopoly/src/host/property/PropertyManager.java
+++ Monopoly/src/host/property/PropertyManager.java
@@ -7,6 +7,10 @@ public class PropertyManager {
 	
 	public PropertyManager(){
 		for(int i = 0; i<40; i++){
+			if(PropertyList.Properties[i][0] == 0){
+				properties.add(new Property(i));
+			}
+			
 			int a = PropertyList.Properties[i][0]; //TODO set up all property stuff
 		}
 	}
