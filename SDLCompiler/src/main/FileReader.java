f830f8965d3d04170d97320f65be5e3d54e43c6d
MatthewLi <MatthewLi@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-02-08 19:57:23 +0000

    so does this work

--- SDLCompiler/src/main/FileReader.java
+++ SDLCompiler/src/main/FileReader.java
@@ -1,7 +1,28 @@
 package main;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileReader {
+	
 	public static String readFile(String file){
-		return "";
+		String endOfLine = System.getProperty("line.separator");
+		List<String> lines = new ArrayList<String>();
+		
+		try {
+			lines = Files.readAllLines(Paths.get(file), Charset.defaultCharset());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (String line : lines) {
+		    sb.append(line).append(endOfLine);
+		}
+		return sb.toString();
 	}
 }
--- SDLCompiler/src/main/SDLCompiler.java
+++ SDLCompiler/src/main/SDLCompiler.java
@@ -1,7 +1,18 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
+
 public class SDLCompiler {
 	public static void main(String[] args){
+		String pathFromCurrentDirectory = "SDLCompiler/src/files/SmartDrive.svg";
+		String path = "";
+		try {
+			path = new File(new File(".").getAbsolutePath()).getCanonicalPath() + "/" + pathFromCurrentDirectory;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+		System.out.println(FileReader.readFile(path));
 	}
 }
