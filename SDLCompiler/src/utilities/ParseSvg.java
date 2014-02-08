7d1e50760291d057c1fca2949f6e023792a0ebd6
SayakChatterjee <SayakChatterjee@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-02-08 19:05:08 +0000

    fixin stuff

--- SDLCompiler/src/utilities/ParseSvg.java
+++ SDLCompiler/src/utilities/ParseSvg.java
@@ -1,10 +1,16 @@
 package utilities;
 
+import java.util.regex.*;
 import javax.xml.parsers.*;
 import org.xml.sax.*;
 import org.xml.sax.helpers.*;
 
 public class ParseSvg {
+    
+    // "M 82.8864,56.6103 A 32.5,32.5 0 0 0 83.6939,85.0485"
+    public static Pattern arcPattern = 
+        // Pattern.compile("([Mm]\\s*([0-9.+-]+),([0-9.+-]+))|([Aa]\\s*([0-9.+-]+),([0-9.+-]+)(\\s*0)*\\s*([0-9.+-]+),([0-9.+-]+))");
+        Pattern.compile("([mM]([ 0-9+-.,]+)[aA]([ 0-9+-.,]+))");
 
     public static void main(String argv[]) {
 
@@ -24,6 +30,19 @@ public class ParseSvg {
 
                     if (qName.equalsIgnoreCase("G")) {
                         started = true;
+                    }
+                    
+                    if (qName.equalsIgnoreCase("PATH")) {
+                        String data = attributes.getValue("d");
+                        System.out.println("***" + data);
+                        Matcher m = arcPattern.matcher(data);
+                        if (m.matches()) {
+                            System.out.print("*** MATCHES *** ");
+                            for (int i = 0; i <= m.groupCount(); i++) {
+                                System.out.printf("\"%s\" ", m.group(i));
+                            }
+                            System.out.println();
+                        }
                     }
 
                     for (int i = 0; i < attributes.getLength(); i++) {
@@ -47,12 +66,10 @@ public class ParseSvg {
                 }
             };
 
-            saxParser.parse("/Users/dcp/temp/test.svg", handler);
+            saxParser.parse("sdl/SmartDrive.svg", handler);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
 }
