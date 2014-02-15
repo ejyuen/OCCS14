73390f77d3550cd0674960260a5917bcf8f55324
AlexYang <AlexYang@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-02-15 19:17:32 +0000

    Added two regex patterns

--- SDLCompiler/src/fileProcessing/Parser.java
+++ SDLCompiler/src/fileProcessing/Parser.java
@@ -4,6 +4,8 @@ import java.util.regex.*;
 import main.SDLCompiler;
 public class Parser {
 	public SDLCompiler sdlCompiler = new SDLCompiler();
+	public static Pattern actionSignalPattern = Pattern.compile("([ 0-9.]+,[ 0-9.]+ ){5}");
+	public static Pattern namePattern = Pattern.compile("x=\"[ 0-9a-z.\"=]+>[A-Za-z ]+");
 	
 	public void addSDLObjects(String XMLText){
 		XMLText = sdlCompiler.getXmlText();
