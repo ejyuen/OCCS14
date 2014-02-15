eb8cf349175d074008094b99ff58fc8be9d0ef3f
SayakChatterjee <SayakChatterjee@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-02-15 19:45:09 +0000

    commiteted

--- SDLCompiler/src/fileProcessing/Parser.java
+++ SDLCompiler/src/fileProcessing/Parser.java
@@ -1,15 +1,30 @@
 package fileProcessing;
+import java.io.File;
+import java.io.IOException;
 import java.util.regex.*;
 
 import main.SDLCompiler;
 public class Parser {
-	public SDLCompiler sdlCompiler = new SDLCompiler();
 	public static Pattern actionSignalPattern = Pattern.compile("([ 0-9.]+,[ 0-9.]+ ){5}");
 	public static Pattern namePattern = Pattern.compile("x=\"[ 0-9a-z.\"=]+>[A-Za-z ]+");
-	
-	public void addSDLObjects(String XMLText){
-		XMLText = sdlCompiler.getXmlText();
-		
+
+	public static void addSDLObjects(String XMLText){
+		System.out.println("Starting");
+		Matcher actionSignalMatcher = actionSignalPattern.matcher(XMLText);
+		if(actionSignalMatcher.matches()){
+			for(int i = 0; i <= actionSignalMatcher.groupCount(); i++){
+				System.out.println("" + actionSignalMatcher.group(i));
+			}
+		}
 	}
+	
+	public static void main (String[] args){
+		String path = "";
+		SDLCompiler compiler = new SDLCompiler();
+		compiler.setXmlText();
+		addSDLObjects(FileReader.readFile(compiler.getXmlText()));
+	}
+	
+
 	//TODO takes in the string from file reader and parses it into and SDL network
 }
