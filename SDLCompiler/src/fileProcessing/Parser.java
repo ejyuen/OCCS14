f363758d5a184979db31f0185845d50998db5573
SayakChatterjee <SayakChatterjee@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-02-15 20:36:32 +0000

    yaay 4 regex

--- SDLCompiler/src/fileProcessing/Parser.java
+++ SDLCompiler/src/fileProcessing/Parser.java
@@ -2,17 +2,20 @@ package fileProcessing;
 import java.util.regex.*;
 
 public class Parser {
-	public static Pattern actionSignalPattern = Pattern.compile("([ 0-9.]+,[ 0-9.]+ ){5}");
+	public static Pattern actionSignalPattern = Pattern.compile("([0-9.,]+ ){5}");
 	public static Pattern namePattern = Pattern.compile("x=\"[ 0-9a-z.\"=]+>[A-Za-z ]+");
 
 	public static void addSDLObjects(String XMLText){
+		
 		System.out.println("Starting");
+		System.out.println("" + XMLText);
 		Matcher actionSignalMatcher = actionSignalPattern.matcher(XMLText);
-		if(actionSignalMatcher.matches()){
-			for(int i = 0; i <= actionSignalMatcher.groupCount(); i++){
-				System.out.println("" + actionSignalMatcher.group(i));
-			}
+	
+		while(actionSignalMatcher.find()){
+			System.out.println("true");
+			System.out.println("" + actionSignalMatcher.group());
 		}
+		
 	}
 
 	//TODO takes in the string from file reader and parses it into and SDL network
