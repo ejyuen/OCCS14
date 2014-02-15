16fe53659f637c56b9135a43a90bd151a1a03f55
SayakChatterjee <SayakChatterjee@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-02-15 21:05:22 +0000

    bootiful

--- SDLCompiler/src/fileProcessing/Parser.java
+++ SDLCompiler/src/fileProcessing/Parser.java
@@ -4,15 +4,24 @@ import java.util.regex.*;
 public class Parser {
 	public static Pattern actionSignalPattern = Pattern.compile("([0-9.,]+ ){5}");
 	public static Pattern namePattern = Pattern.compile("x=\"[ 0-9a-z.\"=]+>[A-Za-z ]+");
+	public static Pattern statePattern = Pattern.compile("(M [0-9.]+ [0-9.]+ L.+z)");
 
 	public static void addSDLObjects(String XMLText){
-		
-		System.out.println("Starting");
-		System.out.println("" + XMLText);
 		Matcher actionSignalMatcher = actionSignalPattern.matcher(XMLText);
+		Matcher stateMatcher = statePattern.matcher(XMLText);
+		Matcher nameMatcher = namePattern.matcher(XMLText);
+		
+		while(stateMatcher.find()){
+			String rawPoints = stateMatcher.group();
+			if(nameMatcher.find(stateMatcher.end())) {
+				String rawName = nameMatcher.group();
+				System.out.println("" + rawPoints + " " + rawName);
+			}
+			
+		}
+		
 	
 		while(actionSignalMatcher.find()){
-			System.out.println("true");
 			System.out.println("" + actionSignalMatcher.group());
 		}
 		
