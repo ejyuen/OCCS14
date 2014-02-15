0750a48ef029a60935200d523f3cae72af7dfcd1
SayakChatterjee <SayakChatterjee@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-02-15 21:40:50 +0000

    made state stuff

--- SDLCompiler/src/fileProcessing/Parser.java
+++ SDLCompiler/src/fileProcessing/Parser.java
@@ -4,7 +4,10 @@ import java.util.regex.*;
 public class Parser {
 	public static Pattern actionSignalPattern = Pattern.compile("([0-9.,]+ ){5}");
 	public static Pattern namePattern = Pattern.compile("x=\"[ 0-9a-z.\"=]+>[A-Za-z ]+");
-	public static Pattern statePattern = Pattern.compile("(M [0-9.]+ [0-9.]+ L.+z)");
+	public static Pattern statePattern = Pattern.compile("L[ 0-9.,LC]+z");
+	public static Pattern xPattern = Pattern.compile(" [0-9.]+");
+	public static Pattern yPattern = Pattern.compile(",[0-9.]+");
+	public static Pattern stringNamePattern = Pattern.compile(">[a-zA-Z]+");
 
 	public static void addSDLObjects(String XMLText){
 		Matcher actionSignalMatcher = actionSignalPattern.matcher(XMLText);
@@ -15,7 +18,35 @@ public class Parser {
 			String rawPoints = stateMatcher.group();
 			if(nameMatcher.find(stateMatcher.end())) {
 				String rawName = nameMatcher.group();
-				System.out.println("" + rawPoints + " " + rawName);
+				Matcher xMatcher = xPattern.matcher(rawPoints);
+				Matcher yMatcher = yPattern.matcher(rawPoints);
+				Matcher stringNameMatcher = stringNamePattern.matcher(rawName);
+				double highestX = 0;
+				double lowestX = Double.MAX_VALUE;
+				double highestY = 0;
+				double lowestY = Double.MAX_VALUE;
+				String name = "default";
+				while(xMatcher.find()){
+					if(Double.parseDouble(xMatcher.group()) > highestX){
+						highestX = Double.parseDouble(xMatcher.group());
+					}
+					if(Double.parseDouble(xMatcher.group()) < lowestX){
+						lowestX = Double.parseDouble(xMatcher.group());
+					}
+				}
+				while(yMatcher.find()){
+					if(Double.parseDouble(yMatcher.group().substring(1)) > highestY){
+						highestY = Double.parseDouble(yMatcher.group().substring(1));
+					}
+					if(Double.parseDouble(yMatcher.group().substring(1)) < lowestY){
+						lowestY = Double.parseDouble(yMatcher.group().substring(1));
+					}
+				}
+				while(stringNameMatcher.find()){
+					name = stringNameMatcher.group().substring(1);
+				}
+				System.out.println("" + name + " " + lowestX + "," + highestX + "," + lowestY + "," + highestY);
+				
 			}
 			
 		}
