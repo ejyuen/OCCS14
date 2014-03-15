3a84cdb0fed46d880f75b27726ed188aa1847f6f
NeilShah <NeilShah@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-03-15 21:45:56 +0000

    Best Timer NA

--- SDLCompiler/src/utilities/Timer.java
+++ SDLCompiler/src/utilities/Timer.java
@@ -1,28 +1,42 @@
 package utilities;
 
+import java.util.ArrayList;
 import java.util.Queue;
 
 public class Timer implements Runnable {
-	
-	private long sleepTime;
-	private Queue<SignalPack> queue = null;
 
-	public static void startTimer(Queue<SignalPack> signalQueue, int miliseconds){
-		new Thread(new Timer(signalQueue, miliseconds)).start();
+	private static ArrayList<Long> endTimes = new ArrayList<Long>();
+	private static boolean endTimer = false; 
+	private Queue<SignalPack> queue = null;
+	
+	public Timer(Queue<SignalPack> signalQueue) {
+		queue = signalQueue;
+		new Thread(this).start();
 	}
 
+	public static void setTimer(int milliseconds){
+		endTimes.add(System.currentTimeMillis() + milliseconds);
+	}
 	
-	public Timer(Queue<SignalPack> signalQueue, long miliseconds){
-		queue = signalQueue;
-		sleepTime = miliseconds;
+	public static void endTimer(){
+		endTimer = true;
 	}
 	
 	public void run() {
-		try {
-			Thread.sleep(sleepTime);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		while(!endTimer){
+			for(int i = 0; i < endTimes.size(); i++){
+				if(System.currentTimeMillis() >= endTimes.get(i)){
+					queue.offer(SignalPack.TIMERDONE);
+					endTimes.remove(i);
+					i--;
+				}
+			}
+			
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
-		queue.offer(SignalPack.TIMERDONE);
 	}
 }
