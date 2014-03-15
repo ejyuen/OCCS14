487e09dfeb69cdd698cdb0932402091958c214bb
MatthewLi <MatthewLi@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-03-15 21:02:00 +0000

    Parsed methodNames and methodParameters by Erica

--- SDLCompiler/src/sdlNetwork/Action.java
+++ SDLCompiler/src/sdlNetwork/Action.java
@@ -2,13 +2,19 @@ package sdlNetwork;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import utilities.ActionPack;
 
 public class Action implements Runnable{
 	private String name;
-	private String[] methodNames;
-	private String[][] methodParameters;
+	private ArrayList<String> methodNames;
+	private ArrayList<String[]> methodParameters;
+	private static Pattern methodPattern = Pattern.compile("[/n][/w]+[(]");
+	private static Pattern parameterPattern = Pattern.compile("[(][^)]+");
+	
 	
 	public Action(String name){
 		this.name = name;
@@ -17,17 +23,37 @@ public class Action implements Runnable{
 	
 	private void init(){
 		//set methodNames and Parameters here;
+		Matcher methodMatcher = methodPattern.matcher(name);
+		Matcher parameterMatcher = parameterPattern.matcher(name);		
+		
+		while(methodMatcher.find()){
+			String rawMethod = methodMatcher.group();
+			rawMethod = rawMethod.substring(1,rawMethod.length()-1);
+			methodNames.add(rawMethod);
+		}
+		
+		while(parameterMatcher.find()){
+			String rawParameter = parameterMatcher.group();
+			String[] parameterArray = rawParameter.split(",");
+			for(int i=0; i < parameterArray.length -1; i++){
+				parameterArray[i].trim();
+			}
+			methodParameters.add(parameterArray);
+		}
+		
+		
+		
 	}
 	
 	public String getName() {
 		return name;
 	}
 	
-	public String[] getMethodNames(){
+	public ArrayList<String> getMethodNames(){
 		return methodNames;
 	}
 	
-	public String[][] getMethodParameters(){
+	public ArrayList<String[]> getMethodParameters(){
 		return methodParameters;
 	}
 	
@@ -35,9 +61,9 @@ public class Action implements Runnable{
 		try {
 			Class<utilities.ActionPack> c = ActionPack.class;
 			
-			for(int i = 0; i < getMethodNames().length; i++){
-				Method method = c.getDeclaredMethod(getMethodNames()[i], String[].class);
-				method.invoke(null, getMethodParameters()[i]);
+			for(int i = 0; i < getMethodNames().size(); i++){
+				Method method = c.getDeclaredMethod(getMethodNames().get(i), String[].class);
+				method.invoke(null, getMethodParameters().get(i));
 			}
 		} catch (NoSuchMethodException e) {
 			// TODO Auto-generated catch block
