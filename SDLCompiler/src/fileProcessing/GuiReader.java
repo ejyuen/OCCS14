079a00c6566e473c885703eb4ddc7a2bbbe95ef0
SayakChatterjee <SayakChatterjee@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-03-15 20:20:24 +0000

    updated diagram to accommodate for a start state

--- SDLCompiler/src/fileProcessing/GuiReader.java
+++ SDLCompiler/src/fileProcessing/GuiReader.java
@@ -1,18 +1,34 @@
 package fileProcessing;
 
-import fileProcessing.sdlGUI.GuiNetwork;
+import fileProcessing.sdlGUI.*;
 import sdlNetwork.*;
 
 public class GuiReader {
-		private GuiNetwork guiNetwork;
-		private SDLNetwork SDLNetwork;
-		
-		public GuiReader(GuiNetwork guiNetwork){
-			this.guiNetwork = guiNetwork;
+	private GuiNetwork guiNetwork;
+	private SDLNetwork SDLNetwork;
+
+	public GuiReader(GuiNetwork guiNetwork, SDLNetwork SDLNetwork){
+		this.guiNetwork = guiNetwork;
+		this.SDLNetwork = SDLNetwork;
+	}
+
+	public void readStates(){
+		for (GuiState gs : guiNetwork.getGuiStates()) {
+			SDLNetwork.addState(new State(gs.getName()));
 		}
-		
-		public void readStates(){
-			
+	}
+	public void readConnections(){
+		State startState;
+		Signal signal;
+		Action action;
+		State endState;
+		for (GuiState gs : guiNetwork.getGuiStates()) {
+			startState = new State(gs.getName());
+			for(GuiConnection gc: guiNetwork.getGuiConnections()){
+				if(gs.intersects(gc.getX1() - 20, gc.getY1() - 20, 40, 40)){
+					//if(new Rectangle2D.Double(gc.getX2() - 20, gc.getY2() - 20 , 40, 40)); 
+				}
+			}
 		}
-		
+	}
 }
