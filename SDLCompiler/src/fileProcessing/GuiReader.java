e63919636853bf9491d501180c1d17a1249c159f
AlexYang <AlexYang@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-03-15 21:23:25 +0000

    gui reader now makes an sdl network from gui network

--- SDLCompiler/src/fileProcessing/GuiReader.java
+++ SDLCompiler/src/fileProcessing/GuiReader.java
@@ -15,7 +15,7 @@ public class GuiReader {
 	}
 
 	public void readStates(){
-		for (GuiState gs : guiNetwork.getGuiStates()) {
+		for(GuiState gs : guiNetwork.getGuiStates()) {
 			if(gs.getName() == "Start"){
 				SDLNetwork.addStartState(new State(gs.getName()));
 			}
@@ -31,11 +31,31 @@ public class GuiReader {
 			startState = new State(gs.getName());
 			for(GuiConnection gc: guiNetwork.getGuiConnections()){
 				if(gs.intersects(gc.getX1() - 20, gc.getY1() - 20, 40, 40)){
-					for(GuiSignal gsl: guiNetwork.getGuiSignals()){
-						if(gsl.intersects(new Rectangle2D.Double(gc.getX2() - 20, gc.getY2() - 20 , 40, 40))){
+					for(GuiSignal gsl : guiNetwork.getGuiSignals()){
+						if(gsl.intersects(gc.getX2() - 20, gc.getY2() - 20 , 40, 40)){
 							signal = new Signal(gsl.getName());
-							for(GuiConnection gc2: guiNetwork.getGuiConnections()){
-								
+							for(GuiConnection gc2 : guiNetwork.getGuiConnections()){
+								if(gsl.intersects(gc2.getX1() - 20, gc.getY1() - 20, 40, 40)){
+									for(GuiAction ga : guiNetwork.getGuiActions()){
+										if(ga.intersects(gc2.getX2() - 20, gc2.getY2() - 20, 40, 40)){
+											action = new Action(ga.getName());
+											for(GuiConnection gc3 : guiNetwork.getGuiConnections()){
+												if(ga.intersects(gc3.getX1() - 20, gc3.getY1() - 20, 40, 40)){
+													for(GuiState gs2 : guiNetwork.getGuiStates()){
+														if(gs2.intersects(gc3.getX2() - 20, gc3.getY2() - 20, 40, 40)){
+															endState = new State(gs2.getName());
+															for(State s : SDLNetwork.getStates()){
+																if(s.equals(startState)){
+																	s.addConnection(new Connection(startState, endState, signal, action));
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
 							}
 						}
 					}
