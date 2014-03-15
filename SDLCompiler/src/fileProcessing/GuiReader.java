7da59f5f40a4c95cbdb8b11f493d4613acebad5c
SayakChatterjee <SayakChatterjee@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-03-15 19:48:07 +0000

    guireaderrr

---
+++ SDLCompiler/src/fileProcessing/GuiReader.java
@@ -0,0 +1,18 @@
+package fileProcessing;
+
+import fileProcessing.sdlGUI.GuiNetwork;
+import sdlNetwork.*;
+
+public class GuiReader {
+		private GuiNetwork guiNetwork;
+		private SDLNetwork SDLNetwork;
+		
+		public GuiReader(GuiNetwork guiNetwork){
+			this.guiNetwork = guiNetwork;
+		}
+		
+		public void readStates(){
+			
+		}
+		
+}
--- SDLCompiler/src/fileProcessing/sdlGUI/GuiConnection.java
+++ SDLCompiler/src/fileProcessing/sdlGUI/GuiConnection.java
@@ -5,13 +5,10 @@ import java.awt.Graphics2D;
 import java.awt.geom.Line2D;
 
 public class GuiConnection extends Line2D.Double {
-
 	private Color color = Color.BLACK;
-	
 	public GuiConnection(double x1, double y1, double x2, double y2){
 		super(x1, y1, x2, y2);
 	}
-	
 	public void paint(Graphics2D g){
 		g.setColor(color);
 		g.draw(this);	
