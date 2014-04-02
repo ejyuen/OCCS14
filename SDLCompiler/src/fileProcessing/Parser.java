e47997f7fecade60ca4c0bdd69a4f8ee5aa6a1e1
AlexYang <AlexYang@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-04-02 02:34:59 +0000

    Fixed a few bugs, cleared up some code

--- SDLCompiler/src/fileProcessing/Parser.java
+++ SDLCompiler/src/fileProcessing/Parser.java
@@ -14,12 +14,11 @@ public class Parser {
 		ACTION, SIGNAL
 	}
 	
-	public static Pattern actionSignalPattern = Pattern.compile("([0-9.,]+ ){5}");
-	public static Pattern namePattern = Pattern.compile("x=\"[ 0-9a-z.\"=]+>[A-Za-z ]+");
+	public static Pattern actionSignalPattern = Pattern.compile("f\" points=\"([0-9.,]+ ){5}");
 	public static Pattern statePattern = Pattern.compile("L[ 0-9.,LC]+z");
 	public static Pattern xPattern = Pattern.compile("( [0-9.]+)(?! )");
 	public static Pattern yPattern = Pattern.compile(",[0-9.]+");
-	public static Pattern stringNamePattern = Pattern.compile(">[a-zA-Z]+");
+	public static Pattern namePattern = Pattern.compile(">[a-zA-Z0-9()]+");
 	public static Pattern connectionPattern = Pattern.compile("M[0-9,. ]+A[0-9,. ]+");
 	private static GuiNetwork network = new GuiNetwork();
 
@@ -32,15 +31,13 @@ public class Parser {
 		while(stateMatcher.find()){
 			String rawPoints = stateMatcher.group();
 			if(nameMatcher.find(stateMatcher.end())) {
-				String rawName = nameMatcher.group();
+				String name = nameMatcher.group().substring(1);
 				Matcher xMatcher = xPattern.matcher(rawPoints);
 				Matcher yMatcher = yPattern.matcher(rawPoints);
-				Matcher stringNameMatcher = stringNamePattern.matcher(rawName);
 				double highestX = 0;
 				double lowestX = Double.MAX_VALUE;
 				double highestY = 0;
 				double lowestY = Double.MAX_VALUE;
-				String name = "default";
 				while(xMatcher.find()){
 					if(Double.parseDouble(xMatcher.group().substring(1)) > highestX){
 						highestX = Double.parseDouble(xMatcher.group().substring(1));
@@ -57,9 +54,6 @@ public class Parser {
 						lowestY = Double.parseDouble(yMatcher.group().substring(1));
 					}
 				}
-				while(stringNameMatcher.find()){
-					name = stringNameMatcher.group().substring(1);
-				}
 				
 				network.addGuiState(new GuiState(name, lowestX, lowestY, highestX-lowestX, highestY-lowestY));
 			}
@@ -67,76 +61,60 @@ public class Parser {
 		}
 		
 		while(actionSignalMatcher.find()){
-			actionSignal type = actionSignal.SIGNAL; // 0 is Signal, 1 is Action, make this an enum im lazy
+			actionSignal type = null; // 0 is Signal, 1 is Action, make this an enum im lazy
 			String rawPoints = actionSignalMatcher.group();
 			Matcher xMatcher = xPattern.matcher(rawPoints);
-			double x2 = 0;
+			double outerXPoint = 0;
+			double innerXPoint = 0;
 			int counter = 0;
 			while(xMatcher.find()){
 				if(counter == 1){
-					x2 = Double.parseDouble(xMatcher.group());
+					outerXPoint = Double.parseDouble(xMatcher.group());
 				}
 				if(counter == 2){
-					if(Double.parseDouble(xMatcher. group()) < x2){
-						type = actionSignal.SIGNAL;
-					}else{
-						type = actionSignal.ACTION;
-					}
+					innerXPoint = Double.parseDouble(xMatcher.group());
 				}
 				counter++;
 			}
+			if(innerXPoint < outerXPoint)
+				type = actionSignal.SIGNAL;
+			else
+				type = actionSignal.ACTION;
+			xMatcher.reset();
+			
 			if(type == actionSignal.SIGNAL){
-				//System.out.println("yoooo");
 				int[] yPoints = new int[5];
-				Matcher xMatcher2 = xPattern.matcher(rawPoints);
 				int xCounter = 0;
 				int xone = 0;
 				int xtwo = 0;
 				int xthree = 0;
-				while(xMatcher2.find()){
+				while(xMatcher.find()){
 					switch(xCounter){
 					case 0:
-						xtwo = (int)(Double.parseDouble(xMatcher2.group().substring(1)));
+						xtwo = (int)(Double.parseDouble(xMatcher.group().substring(1)));
 						break;
 					case 1:
-						xthree = (int)(Double.parseDouble(xMatcher2.group().substring(1)));
+						xthree = (int)(Double.parseDouble(xMatcher.group().substring(1)));
 						break;
 					case 3:
-						xone  = (int)(Double.parseDouble(xMatcher2.group().substring(1)));
+						xone  = (int)(Double.parseDouble(xMatcher.group().substring(1)));
 						break;
 					default:
 						break;
 					}
 					xCounter++;
 				}
-				//System.out.println(xone + " " +  xtwo + " " + xthree + " " + xtwo + " " + xone);
 				int[] xPoints = {xone, xtwo, xthree, xtwo, xone};
 				Matcher yMatcher2 = yPattern.matcher(rawPoints);
 				int yIndex = 0;
 				while(yMatcher2.find()){
 					yPoints[yIndex] = (int)(Double.parseDouble(yMatcher2.group().substring(1)));
-					//System.out.println((int)(Double.parseDouble(yMatcher2.group().substring(1))));
 					yIndex++;
 				}
-				//System.out.println(yPoints[0] + " " + yPoints[1] + " " + yPoints[2] + " " + yPoints[3] + " " + yPoints[4]);
-				String name = "default";
 				if(nameMatcher.find(actionSignalMatcher.end())) {
-					String rawName = nameMatcher.group();
-					Matcher stringNameMatcher = stringNamePattern.matcher(rawName);
-					while(stringNameMatcher.find()){
-						name = stringNameMatcher.group().substring(1);
-					}
-					boolean before = false;
-					for(GuiAction ga: network.getGuiActions()){
-						if(ga.getName().equals(name)){
-							before = true;
-						}
-					}
-					if(!before){
+					String name = nameMatcher.group().substring(1);
 					network.addGuiAction(new GuiAction(name, xPoints, yPoints));
-					}
 				}
-				/////start coding from here
 			}
 			if(type == actionSignal.ACTION){
 				int[] yPoints = new int[5];
@@ -168,22 +146,9 @@ public class Parser {
 					yPoints[yIndex] = (int)(Double.parseDouble(yMatcher2.group().substring(1)));
 					yIndex++;
 				}
-				String name = "default";
 				if(nameMatcher.find(actionSignalMatcher.end())) {
-					String rawName = nameMatcher.group();
-					Matcher stringNameMatcher = stringNamePattern.matcher(rawName);
-					while(stringNameMatcher.find()){
-						name = stringNameMatcher.group().substring(1);
-					}
-					boolean befor3 = false;
-					for(GuiSignal ga: network.getGuiSignals()){
-						if(ga.getName().equals(name)){
-							befor3 = true;
-						}
-					}
-					if(!befor3){
+					String name = nameMatcher.group().substring(1);
 					network.addGuiSignal(new GuiSignal(name, xPoints, yPoints));
-					}
 				}
 			}
 		}
