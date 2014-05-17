342c599731aec65b1110cd0a4614647a1b0a5fc0
MatthewLi <MatthewLi@e0ab1361-b0f7-43da-99f6-020dc8f6e263>
2014-05-17 18:24:41 +0000

    comment change in action pack

--- SDLCompiler/src/utilities/ActionPack.java
+++ SDLCompiler/src/utilities/ActionPack.java
@@ -2,8 +2,6 @@ package utilities;
 
 /**
  * This is the class that contains all the methods that are in a state machine
- * All public methods should be in the form: 
- * 		public static void methodName(String[] params)
  * 
  * It's important to note that the method must have 1 parameter that is a String array
  * 
